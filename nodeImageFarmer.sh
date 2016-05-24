72247ac995b29d86a91a7865eb5c9b21eee55f05
Allan Bogh <abogh@hollandamerica.com>
2016-05-24 16:11:57 -0700

    Modified the nodeImageFarmer service so it logs and nohups.

--- nodeImageFarmer.sh
+++ nodeImageFarmer.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-npm run app
+DIRECTORY="/var/log/node-image-farmer"
+
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir $DIRECTORY
+fi
+
+if [ -d "$DIRECTORY" ]; then
+  nohup ./imageFarmerService.sh > /var/log/node-image-farmer/node-image-farmer.log 2>&1 &
+fi
